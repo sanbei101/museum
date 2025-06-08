4c37271728bbd7b81aaad4602170b9bd09ffb4c5
sanbei101 <sanbei101@outlook.com>
2025-06-08 11:55:29 +0800

    替换后端逻辑

--- src/api/artifact.ts
+++ src/api/artifact.ts
@@ -1,11 +1,14 @@
 import { type Artifact, type Page } from './type';
-import { pb } from './pocketbase';
-const FetchArtifact = async (page: number, limit: number): Promise<Page<Artifact>> => {
-  const resultList = await pb.collection('artifacts').getList(page, limit, {
-    sort: '-created'
-  });
+import axiosInstance from './axios';
 
-  return resultList as unknown as Page<Artifact>;
+const FetchArtifact = async (page: number, perPage: number): Promise<Page<Artifact>> => {
+  const response = await axiosInstance.get(`/artifacts`, {
+    params: {
+      page,
+      perPage
+    }
+  });
+  return response.data as Page<Artifact>;
 };
 
 export { FetchArtifact };
--- src/api/pocketbase.ts
+++
@@ -1,4 +0,0 @@
-import PocketBase from 'pocketbase';
-
-const POCKETBASE_URL = import.meta.env.VITE_POCKETBASE_URL as string;
-export const pb = new PocketBase(POCKETBASE_URL);
